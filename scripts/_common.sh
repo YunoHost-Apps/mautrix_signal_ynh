933bd4c6444107b12a32fbdf9b0f3617346a38b6
Gredin 67 <gredin67@sans-nuage.fr>
2021-01-23 21:21:10 +0100

    correct pip3 and python3

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql"
+pkg_dependencies="postgresql python3"
 
 #=================================================
 # PERSONAL HELPERS
