b3a1ef465c40d1f821de7a7f7034544cfdec031d
Gredin67 <gredin67@sans-nuage.fr>
2021-08-08 01:20:38 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,8 +5,8 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql python3 default-jre-headless"
-extra_dependencies="libunixsocket-java signaldctl"
+pkg_dependencies="postgresql python3"
+extra_dependencies="libunixsocket-java signald signaldctl"
 #=================================================
 # PERSONAL HELPERS
 #=================================================
