fc1ba62e6529bb529a413d5895398baa5f2029d7
Mayeul Cantan <oss@mayeul.net>
2021-10-29 00:53:29 +0200

    Pin dependency signald>=0.15.0-27
    Starting with -27, the libc issue should be solved and the package
    should work again on buster.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 # dependencies used by the app
 pkg_dependencies="postgresql python3"
-extra_dependencies="libunixsocket-java signald signaldctl"
+extra_dependencies="libunixsocket-java signald>=0.15.0-27 signaldctl"
 #=================================================
 # PERSONAL HELPERS
 #=================================================
