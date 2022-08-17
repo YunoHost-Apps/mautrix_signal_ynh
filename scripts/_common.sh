23d84702b544554707d0547d5a7c1c97ad8764ad
rosbeef andino <romain.raynaud@3cmr.fr>
2022-08-17 21:34:37 +0000

    add support armhf
    need rustc to compile weel 32b

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,6 +7,13 @@
 # dependencies used by the app
 pkg_dependencies="postgresql python3 python3-dev build-essential"
 extra_dependencies="libunixsocket-java signald signaldctl"
+
+# rustc is mandatory as weel is not existent for arm32 bits
+if [ $YNH_ARCH == "armhf" ] 
+then
+	pkg_dependencies_arm="rustc"
+	pkg_dependencies="$pkg_dependencies $pkg_dependencies_arm"
+fi
 #=================================================
 # PERSONAL HELPERS
 #=================================================
